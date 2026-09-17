6bdd9d3b109f6d89c7f156ca7034d8d451aacc18
Francisco Samaca <pachosp123@gmail.com>
2026-09-16 21:06:33 -0500

    feat: Implementamos conexion JDBC con MySQL

--- src/gestionestudiantes/conexion/Conexion.java
+++ src/gestionestudiantes/conexion/Conexion.java
@@ -4,10 +4,45 @@
  */
 package gestionestudiantes.conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author OMEN
  */
 public class Conexion {
+    private final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final String URL = "jdbc:mysql://localhost:3306/";
+     private final String DB = "dbsistema";
+    private final String USER = "root";
+    private final String PASSWORD = "";
     
+    // Declarar una variable que almacerá la única instancia de Conexion
+    private static Conexion instancia;
+    
+    // Creamos un constructor privado
+    // Al ser private ningua otra clase podrá escribir new Conexion()
+    private Conexion(){
+        
+    }
+    
+    // Creamos un método público y estático para obtener la instacia.
+    public static Conexion getInstancia(){
+        // Verificamos si todavía no hemos creado una instancia
+        if(instancia == null){
+            // Creamos la isntancia únicamente la primera vez
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+    
+    public Connection conectar() throws SQLException{
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD
+        );
+    }
 }
---
+++ src/gestionestudiantes/conexion/PruebaConexion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestionestudiantes.conexion;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author fsamaca
+ */
+public class PruebaConexion {
+    public static void main(String[] args) {
+        try{
+            Connection conexion = Conexion.getInstancia().conectar();
+            if(conexion != null){
+                System.out.println("Conexión exitosa con MySQL");
+                conexion.close();
+            }
+        }catch(SQLException e){
+            System.out.println("Error de conexion: " + e.getMessage());
+        }
+    }
+}
